979aa2ee81a401914304ffc911d19faf516171d0
Abraham Hsu <smokiebacon@gmail.com>
2020-05-29 21:38:06 -0700

    adds internationalization

---
+++ src/layout/AppFooter.vue
@@ -0,0 +1,83 @@
+<template>
+  <footer class="footer has-cards">
+    <div class="container">
+      <div class="row row-grid align-items-center my-md">
+        <div class="col-lg-6">
+          <h4 class="mb-0 font-weight-light">
+            Or get in touch on any of these platforms.
+          </h4>
+        </div>
+        <div class="col-lg-6 text-lg-center btn-wrapper">
+          <a
+            target="_blank"
+            rel="noopener"
+            href=""
+            class="btn btn-neutral btn-icon-only btn-twitter btn-round btn-lg"
+            data-toggle="tooltip"
+            data-original-title="Follow us"
+          >
+            <i class="fa fa-weixin"></i>
+          </a>
+          <a
+            target="_blank"
+            rel="noopener"
+            href=""
+            class="btn btn-neutral btn-icon-only btn-facebook btn-round btn-lg"
+            data-toggle="tooltip"
+            data-original-title="Like us"
+          >
+            <i class="fa fa-facebook-square"></i>
+          </a>
+          <a
+            target="_blank"
+            rel="noopener"
+            href=""
+            class="btn btn-neutral btn-icon-only btn-lg btn-round"
+            data-toggle="tooltip"
+            data-original-title="Follow us"
+          >
+            <i class="fa fa-line"></i>
+          </a>
+        </div>
+      </div>
+      <hr />
+      <div class="row align-items-center justify-content-md-between">
+        <div class="col-md-6">
+          <div class="copyright">
+            &copy; {{ year }}
+            <a
+              href="https://www.zbdlmedicalgroups.com"
+              target="_blank"
+              rel="noopener"
+              >ZBDL Medical Groups</a
+            >
+          </div>
+        </div>
+        <div class="col-md-6">
+          <ul class="nav nav-footer justify-content-end">
+            <li class="nav-item">
+              <a
+                href="http://abrahamhsu.com/"
+                class="nav-link"
+                target="_blank"
+                rel="noopener"
+                >Website created by Abraham Hsu</a
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+<script>
+export default {
+  name: 'app-footer',
+  data() {
+    return {
+      year: new Date().getFullYear()
+    }
+  }
+}
+</script>
+<style></style>
---
+++ src/layout/AppHeader.vue
@@ -0,0 +1,157 @@
+<template>
+  <header class="header-global">
+    <base-nav class="navbar-main" transparent type="" effect="light" expand>
+      <router-link slot="brand" class="navbar-brand mr-lg-5" to="/">
+        <img src="img/brand/zbdl-logo.png" alt="logo" />
+      </router-link>
+
+      <div class="row" slot="content-header" slot-scope="{ closeMenu }">
+        <div class="col-6 collapse-brand">
+          <a
+            href="https://demos.creative-tim.com/vue-argon-design-system/documentation/"
+          >
+            <img src="img/brand/blue.png" />
+          </a>
+        </div>
+        <div class="col-6 collapse-close">
+          <close-button @click="closeMenu"></close-button>
+        </div>
+      </div>
+
+      <ul class="navbar-nav navbar-nav-hover align-items-lg-center">
+        <base-dropdown class="nav-item" menu-classes="dropdown-menu-xl">
+          <a
+            slot="title"
+            href="#"
+            class="nav-link"
+            data-toggle="dropdown"
+            role="button"
+          >
+            <i class="ni ni-ui-04 d-lg-none"></i>
+            <span class="nav-link-inner--text">Products</span>
+          </a>
+          <div class="dropdown-menu-inner">
+            <a
+              href="https://zbdlmedicalgroups.com/facemasks"
+              class="media d-flex align-items-center"
+            >
+              <div
+                class="icon icon-shape bg-gradient-primary rounded-circle text-white"
+              >
+                <i class="ni ni-spaceship"></i>
+              </div>
+              <div class="media-body ml-3">
+                <h6 class="heading text-primary mb-md-1">Facemasks</h6>
+                <p class="description d-none d-md-inline-block mb-0">
+                  Protection from micro-particles and flying debris.
+                </p>
+              </div>
+            </a>
+            <a
+              href="https://zbdlmedicalgroups.com/faceshields"
+              class="media d-flex align-items-center"
+            >
+              <div
+                class="icon icon-shape bg-gradient-warning rounded-circle text-white"
+              >
+                <i class="ni ni-ui-04"></i>
+              </div>
+              <div class="media-body ml-3">
+                <h5 class="heading text-warning mb-md-1">Face Shields</h5>
+                <p class="description d-none d-md-inline-block mb-0">
+                  Protection from micro-particles and dust.
+                </p>
+              </div>
+            </a>
+          </div>
+        </base-dropdown>
+      </ul>
+      <ul class="navbar-nav align-items-lg-center ml-lg-auto">
+        <li class="nav-item">
+          <a
+            class="nav-link nav-link-icon"
+            href="https://www.facebook.com/zbdlmedicalgroups"
+            target="_blank"
+            rel="noopener"
+            data-toggle="tooltip"
+            title="Like us on Facebook"
+          >
+            <i class="fa fa-facebook-square"></i>
+            <span class="nav-link-inner--text d-lg-none">Facebook</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a
+            class="nav-link nav-link-icon"
+            href="https://www.instagram.com/zbdlmedicalgroups"
+            target="_blank"
+            rel="noopener"
+            data-toggle="tooltip"
+            title="Follow us on Instagram"
+          >
+            <i class="fa fa-instagram"></i>
+            <span class="nav-link-inner--text d-lg-none">Instagram</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a
+            class="nav-link nav-link-icon"
+            href="https://twitter.com/zbdlmedicalgroups"
+            target="_blank"
+            rel="noopener"
+            data-toggle="tooltip"
+            title="Follow us on Twitter"
+          >
+            <i class="fa fa-twitter-square"></i>
+            <span class="nav-link-inner--text d-lg-none">Twitter</span>
+          </a>
+        </li>
+        <li class="nav-item d-none d-lg-block ml-lg-4">
+          <a
+            href=""
+            target="_blank"
+            rel="noopener"
+            class="btn btn-neutral btn-icon"
+          >
+            <span class="btn-inner--icon">
+              <i class="fa fa-cloud-download mr-2"></i>
+            </span>
+            <span class="nav-link-inner--text">Contact Us</span>
+          </a>
+        </li>
+      </ul>
+      <div>
+        <base-dropdown>
+          <base-button slot="title" type="secondary" class="dropdown-toggle">
+            <img
+              src="https://demos.creative-tim.com/argon-design-system/assets/img/icons/flags/US.png"
+            />
+            ENGLISH
+          </base-button>
+          <li>
+            <a class="dropdown-item" href="#">
+              <img
+                src="https://demos.creative-tim.com/argon-design-system/assets/img/icons/flags/CH.png"
+              />
+              CHINESE
+            </a>
+          </li>
+        </base-dropdown>
+      </div>
+    </base-nav>
+  </header>
+</template>
+<script>
+import BaseNav from '@/components/BaseNav'
+import BaseDropdown from '@/components/BaseDropdown'
+import CloseButton from '@/components/CloseButton'
+
+export default {
+  components: {
+    BaseNav,
+    CloseButton,
+    BaseDropdown
+  }
+}
+</script>
+<style></style>
